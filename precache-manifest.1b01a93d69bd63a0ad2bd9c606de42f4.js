98434501b36b988a7bad1505b414bd2a1d471f54
MrBusiness87 <bbartula87@gmail.com>
2020-11-11 09:51:39 -0500

    Updates

---
+++ precache-manifest.1b01a93d69bd63a0ad2bd9c606de42f4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c08f64b37352a2af2727a207b19895",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "74741587a532435d1acb",
+    "url": "/My-Portfolio/static/js/2.66421c61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My-Portfolio/static/js/2.66421c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0e5ca05ceef682a95b1",
+    "url": "/My-Portfolio/static/js/main.17915fbf.chunk.js"
+  },
+  {
+    "revision": "bd26aa8a63ab866d9120",
+    "url": "/My-Portfolio/static/js/runtime-main.295a29ee.js"
+  },
+  {
+    "revision": "2fd98bd954c61c08c68bd01e9b552cb8",
+    "url": "/My-Portfolio/static/media/Google thumbnail.2fd98bd9.jpg"
+  },
+  {
+    "revision": "b19f54ddefa41d5fb81e310bd4a3f038",
+    "url": "/My-Portfolio/static/media/LINKEDIN thumbnail.b19f54dd.jpg"
+  },
+  {
+    "revision": "672795767ab1b397ce63c3483e219413",
+    "url": "/My-Portfolio/static/media/NOTE thumbnail.67279576.png"
+  },
+  {
+    "revision": "b57aa82f40641d4765479ddd1e729465",
+    "url": "/My-Portfolio/static/media/Project2.b57aa82f.png"
+  },
+  {
+    "revision": "185a7b9bfa774c18dc7758b5d66b5c25",
+    "url": "/My-Portfolio/static/media/QUIZ thumbnail.185a7b9b.png"
+  },
+  {
+    "revision": "4cdcacb23b7313f0ae916d4234313451",
+    "url": "/My-Portfolio/static/media/READMEGEN thumbnail.4cdcacb2.png"
+  },
+  {
+    "revision": "fcd962993dab29921f5749a0cac15e1e",
+    "url": "/My-Portfolio/static/media/SF-thumbnail.fcd96299.jpg"
+  },
+  {
+    "revision": "7a08751a2c388e8bfb0819dcda10eb89",
+    "url": "/My-Portfolio/static/media/SQL_Employee_Tracker_GIF.7a08751a.gif"
+  },
+  {
+    "revision": "54b20d26ffb4b2c1d48f24ec8ba4aa1d",
+    "url": "/My-Portfolio/static/media/Scratch thumbnail.54b20d26.jpg"
+  },
+  {
+    "revision": "438ffb6c0deee256757bc5a37b0d05fe",
+    "url": "/My-Portfolio/static/media/TEMPLATE thumbnail.438ffb6c.png"
+  },
+  {
+    "revision": "c287fbee5325a0bd680d2e638116cdb1",
+    "url": "/My-Portfolio/static/media/WEATHER thumbnail.c287fbee.png"
+  },
+  {
+    "revision": "afdd124f5fcb607c9cdbacb95cd276a5",
+    "url": "/My-Portfolio/static/media/WORKDAY thumbnail.afdd124f.png"
+  },
+  {
+    "revision": "626b13215cc0427cbe17415bdd3ea7ea",
+    "url": "/My-Portfolio/static/media/candp.626b1321.jpg"
+  },
+  {
+    "revision": "47c9ef417673dd534a3e686a844dd433",
+    "url": "/My-Portfolio/static/media/nodejs.47c9ef41.png"
+  },
+  {
+    "revision": "17b035f1fb9b99efb7cb75195f829ac8",
+    "url": "/My-Portfolio/static/media/profile.17b035f1.jpg"
+  }
+]);
